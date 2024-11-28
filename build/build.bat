ef0408a2f6fe988db457a0b5f6b64ded6ac624f6
diegomrno <diegomoreno.pro45@gmail.com>
2024-11-29 00:30:44 +0100

    feat: Windows support for utilities

--- build/build.bat
+++ build/build.bat
@@ -1,32 +1,88 @@
 @echo off
+setlocal
 
 mkdir build_spdlog
 mkdir build
 
 cd build_spdlog
-cmake -G "MinGW Makefiles" ..\..\lib\spdlog
-mingw32-make.exe -j%NUMBER_OF_PROCESSORS%
+call cmake.exe ../../lib/spdlog
+call nmake -j%NUMBER_OF_PROCESSORS%
 
 cd ..\build
-cmake -G "MinGW Makefiles" ..\..
-mingw32-make.exe -j%NUMBER_OF_PROCESSORS%
+call cmake.exe ..\..
+call nmake -j%NUMBER_OF_PROCESSORS%
 
 cd ..
-
 mkdir build\dist
-copy ..\misc\windows\icon.png build\bin\
-copy ..\misc\windows\main.py build\bin\
-copy ..\misc\windows\admin_manifest.xml build\bin\
 
+copy ..\misc\windows\installer\icon.png build\bin\
+copy ..\misc\windows\installer\main.py build\bin\
+copy ..\misc\windows\installer\admin_manifest.xml build\bin\
 cd build\bin
 
-pyinstaller --onefile --name VortexInstaller --icon=icon.png --add-data "vortex_installer.exe;." --add-data "ressources;ressources" --add-binary "libglm_shared.dll;." --add-binary "librestclient-cpp.dll;." --add-binary "libspdlog.dll;." --add-binary "SDL2.dll;." --manifest=admin_manifest.xml main.py
+call pyinstaller --onefile --name VortexInstaller --icon=icon.png ^
+    --add-data "vortex_installer.exe;." ^
+    --add-data "ressources;ressources" ^
+    --manifest=admin_manifest.xml ^
+    main.py
 
-cd ..
-cd ..
+del VortexInstaller.spec
+del icon.png main.py
+cd ..\..
 
-mkdir shipping\windows
+copy ..\misc\windows\updater\icon.png build\bin\
+copy ..\misc\windows\updater\main.py build\bin\
+copy ..\misc\windows\updater\admin_manifest.xml build\bin\
+cd build\bin
 
-copy build\bin\dist\VortexInstaller.exe shipping\windows\
+call pyinstaller --onefile --name VortexUpdater --icon=icon.png ^
+    --add-data "vortex_update.exe;." ^
+    --add-data "ressources;ressources" ^
+    --manifest=admin_manifest.xml ^
+    main.py
 
-:: TODO : "Publish" the app on the system
+del icon.png main.py org.vortex.updater.policy
+cd ..\..
+
+copy ..\misc\windows\vxuninstaller\icon.png build\bin\
+copy ..\misc\windows\vxuninstaller\main.py build\bin\
+copy ..\misc\windows\vxuninstaller\admin_manifest.xml build\bin\
+cd build\bin
+
+call pyinstaller --onefile --name VersionUninstaller --icon=icon.png ^
+    --add-data "vxuninstall.exe;." ^
+    --add-data "ressources;ressources" ^
+    --manifest=admin_manifest.xml ^
+    main.py
+
+del icon.png main.py
+cd ..\..
+
+copy ..\misc\windows\vxinstaller\icon.png build\bin\
+copy ..\misc\windows\vxinstaller\main.py build\bin\
+copy ..\misc\windows\vxinstaller\admin_manifest.xml build\bin\
+cd build\bin
+
+call pyinstaller --onefile --name VersionInstaller --icon=icon.png ^
+    --add-data "vxinstaller.exe;." ^
+    --add-data "ressources;ressources" ^
+    --manifest=admin_manifest.xml ^
+    main.py
+
+del icon.png main.py
+cd ..\..
+
+copy build\bin\dist\* build\bin
+rd /s /q build\bin\build
+rd /s /q build\bin\dist
+del build\bin\vortex_installer
+del build\bin\vortex_update
+del build\bin\org.vortex.vxinstaller.policy
+del build\bin\VortexUpdater.spec
+del build\bin\VersionInstaller.spec
+del build\bin\VersionUninstaller.spec
+
+mkdir shipping\linux
+copy build\bin\dist\VortexInstaller shipping\windows\
+
+endlocal
---
+++ misc/windows/updater/main.py
@@ -0,0 +1,32 @@
+import ctypes
+import os
+import sys
+import subprocess
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def main():
+    if not is_admin():
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+        sys.exit()
+
+    if getattr(sys, 'frozen', False):
+        app_path = sys._MEIPASS
+    else:
+        app_path = os.path.dirname(os.path.abspath(__file__))
+
+    exe_path = os.path.join(app_path, "vortex_update.exe")
+
+    try:
+        subprocess.run([exe_path], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error while executing the Vortex Updater: {exe_path}: {e}")
+    except FileNotFoundError:
+        print(f"Executable not found: {exe_path}")
+
+if __name__ == "__main__":
+    main()
