1899f8733ad89e4e5aa3a783c9e6d008e66224a1
diegomrno <diegomoreno.pro45@gmail.com>
2024-11-29 11:43:03 +0100

    feat: Add managment of params in utils

--- misc/windows/vxuninstaller/main.py
+++ misc/windows/vxuninstaller/main.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 
 def is_admin():
+    """Check if the script is running with administrator privileges."""
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
@@ -11,7 +12,12 @@ def is_admin():
 
 def main():
     if not is_admin():
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+        print("This application needs to run as administrator to uninstall Vortex.")
+        try:
+            params = " ".join(f'"{arg}"' for arg in sys.argv)
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, params, None, 1)
+        except Exception as e:
+            print(f"Error while trying to elevate privileges: {e}")
         sys.exit()
 
     if getattr(sys, 'frozen', False):
@@ -21,12 +27,14 @@ def main():
 
     exe_path = os.path.join(app_path, "vxuninstaller.exe")
 
+    uninstaller_args = [exe_path] + sys.argv[1:]
+
     try:
-        subprocess.run([exe_path], check=True)
+        subprocess.run(uninstaller_args, check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Error while executing the Vortex Version Uninstaller: {exe_path}: {e}")
+        print(f"Error while executing Vortex Uninstaller: {exe_path}: {e}")
     except FileNotFoundError:
-        print(f"Executable not found: {exe_path}")
+        print(f"Executable file not found: {exe_path}")
 
 if __name__ == "__main__":
     main()
